b9d05973835cc03dc649707e4951782f18f11efd
Sam Meech-Ward <sam@meech-ward.com>
2019-05-28 13:33:52 +0100

    Rest file to Richard Stanley's

--- Install-Node.sh
+++ Install-Node.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# Original version written by Richard Stanley (audstanley)
-
+# written by Richard Stanley (audstanley);
 PICHIP=$(uname -m);
 MAJOR_NODEJS_VERSION=v12
 if [ "$EUID" -ne 0 ]
@@ -12,22 +11,16 @@ NODEFOLDER=$(echo $LINKTONODE | sed 's/.tar.gz/\//');
 #Next, Creates directory for downloads, and downloads node 8.x
 cd ~/ && mkdir tempNode && cd tempNode && wget https://nodejs.org/dist/latest-${MAJOR_NODEJS_VERSION}.x/$LINKTONODE;
 tar -xzf $LINKTONODE;
-
-# Remove the tar after downloading
+#Remove the tar after extracing it.
 rm $LINKTONODE;
-
-# Remove older version of node:
+#remove older version of node:
 rm -R -f /opt/nodejs/;
-
-# Remove symlinks
+#remove symlinks
 rm /usr/bin/node /usr/sbin/node /sbin/node /sbin/node /usr/local/bin/node /usr/bin/npm /usr/sbin/npm /sbin/npm /usr/local/bin/npm 2> /dev/null;
-
-# Copy Node over to the appropriate folder.
+#This next line will copy Node over to the appropriate folder.
 mv ~/tempNode/$NODEFOLDER /opt/nodejs/;
-
-# Remove the nodeJs tar we downloaded.
+#This line will remove the nodeJs tar we downloaded.
 rm -R -f ~/tempNode/$LINKTONODE/;
-
 #Create symlinks to node && npm
 update-alternatives --install "/usr/bin/node" "node" "/opt/nodejs/bin/node" 1
 update-alternatives --install "/usr/bin/npm" "npm" "/opt/nodejs/bin/npm" 1
@@ -36,6 +29,7 @@ rm -R -f /root/tempNode/;
 # Get the python installer (for specific versions):
 cd /bin/;
 rm node-install;
-wget https://raw.githubusercontent.com/meech-ward/NodeJs-Raspberry-Pi/master/node-install;
+wget https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/node-install;
 chmod +x node-install;
 
+fi
